36fbd4c8f90d86ff28955875909c095c70cd6d06
steph <boli@boli.local>
2018-03-29 15:40:53 -0700

    adding hello.php

---
+++ Hello.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>IntroPHP</title>
+</head>
+<body> 
+	<?php  // The tag that begins the PHP code section
+
+			$user_name = "Steph";
+
+			echo "I am $user_name";
+
+			echo '<br>';
+
+			echo 'I am $user_name';
+
+			//echo "Hello $user_name, Welcome to the world of PHP!";  
+			// echo simply outputs the string passed to it	
+			// The tag that ends the PHP code section
+	?>  
+</body>
+</html>
